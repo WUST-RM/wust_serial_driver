80ea8fce72c497c4972fddcb246ab27d3823157e
HYxiaojian <165478586+hyheiyue@users.noreply.github.com>
2025-02-24 19:09:30 +0800

    Update serial_driver.launch.py

--- launch/serial_driver.launch.py
+++ launch/serial_driver.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
         get_package_share_directory('rm_serial_driver'), 'config', 'serial_driver.yaml')
 
     rm_serial_driver_node = Node(
-        package='rm_serial_driver',
+        package='wust_serial_driver',
         executable='rm_serial_driver_node',
         namespace='',
         output='screen',
